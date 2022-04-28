b229a6a785a93888b0f6d1f49e957d751099821d
Guillermo Mora <guillermo@cleverppc.com>
2022-04-28 09:34:35 +0200

    Start source code

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.1.0'
+
+gem 'http'
+gem 'puma'
+gem 'roda'
---
+++ app.rb
@@ -0,0 +1,24 @@
+require "roda"
+require "http"
+
+class App < Roda
+  route do |r|
+    # /
+    r.root do
+      r.redirect "/docs"
+    end
+
+    # /docs
+    r.get "docs" do
+      "Use /joke to get a random joke."
+    end
+
+    # /joke
+    r.get "joke" do
+      http = HTTP.get("http://api.icndb.com/jokes/random")
+      joke = JSON.parse(http.body.to_s)["value"]["joke"]
+      
+      "Here's a random joke: #{joke}"
+    end
+  end
+end
